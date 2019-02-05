29b22e765814f42b22fac8339d6ea6f3535f5eee
Jorge Vásquez Pérez <yorch.vasquez@gmail.com>
2019-02-05 15:32:49 -0800

    Added a tool to flatten a folder's contents in the AFX project.

--- ScriptUI Panels/yToolBox.jsx
+++ ScriptUI Panels/yToolBox.jsx
@@ -120,10 +120,12 @@ YTB.init();
 
 #include "../yScripts/y_setStructure.jsx";
 #include "../yScripts/y_CompHerder.jsx";
+#include "../yScripts/y_flattenSelectedFolderContents.jsx";
 #include "../yScripts/RenderToProject.jsx";
 #include "../yScripts/y_colorProbe.jsx";
 #include "../yScripts/y_BindBetween2.jsx";
 #include "../yScripts/y_CPOffset.jsx";
 #include "../yScripts/y_puppetRig.jsx";
 #include "../yScripts/y_stopMotionLayer.jsx";
+
 
